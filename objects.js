67c5482ebdf31da431e98bdd66065573cef2fc99
JINOOK JUNG <jinook929@gmail.com>
2020-11-06 01:23:00 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,10 +1,18 @@
+<<<<<<< HEAD
 var playlist = { Beatles: "Yesterday", Slowdive: "No"}
+=======
+var playlist = {}
+>>>>>>> b77e976de8c54e06614cca271cc544d4835e7935
 
 function updatePlaylist (playlist, name, title) {
   return Object.assign(playlist, {[name]: title})
 }
 
 function removeFromPlaylist (playlist, name) {
+<<<<<<< HEAD
   delete playlist[name]
   return playlist
+=======
+  return delete playlist.name
+>>>>>>> b77e976de8c54e06614cca271cc544d4835e7935
 }
